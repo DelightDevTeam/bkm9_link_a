e519ed7adbbf51ec6929af8d88c3347c9e24b18f
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-07-27 14:34:15 +0630

    first_commit

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -10,7 +10,7 @@
             <div class="card z-index-0">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">BKM9 Sign in</h4>
                 </div>
               </div>
               <div class="card-body">
