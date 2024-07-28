695544d41c34014c57f8302e9646488249f97704
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-28 09:19:19 +0700

    a

--- app/Models/Admin/ContactLink.php
+++ app/Models/Admin/ContactLink.php
@@ -16,4 +16,26 @@ class ContactLink extends Model
         'telegram',
         'messager'
     ];
+    protected $appends = [ 
+        'viber_link',
+        'telegram_link',
+        'messenger_link', 
+        'facebook_link',
+        'line_link', 
+    ];
+    public function getMessengerLinkAttribute(){
+        return 'https://www.messenger.com/t/'.$this->messager;
+    }
+    public function getLineLinkAttribute(){
+        return 'https://line.me/ti/p/'.$this->line;
+    }
+    public function getViberLinkAttribute(){
+        return 'viber://chat?number='.$this->viber;
+    }
+    public function getTelegramLinkAttribute(){
+        return 'https://t.me/'.$this->telegram;
+    }
+    public function getFacebookLinkAttribute(){
+        return 'https://www.facebook.com/'.$this->facebook_page;
+    }
 }
