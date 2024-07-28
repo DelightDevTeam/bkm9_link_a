3624c6850aae0056194faec90f70e9c59ac9c9d8
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-07-28 08:47:26 +0630

    link updated

--- resources/views/admin/contact/edit.blade.php
+++ resources/views/admin/contact/edit.blade.php
@@ -62,7 +62,7 @@
 <div class="row">
   <div class="col-12">
     <div class="container mb-3">
-      <a class="btn btn-icon btn-2 btn-primary float-end me-5" href="{{ route('admin.games.index') }}">
+      <a class="btn btn-icon btn-2 btn-primary float-end me-5" href="{{ route('admin.contact-links.index') }}">
         <span class="btn-inner--icon mt-1"><i class="material-icons">arrow_back</i>Back</span>
       </a>
     </div>
