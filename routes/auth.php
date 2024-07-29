8e8ab37aabb617898be276bae9b4a0ea683a0660
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-29 14:48:36 +0700

    a

--- routes/auth.php
+++ routes/auth.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 //auth routes
-Route::get('/', [AuthController::class, 'showLogin']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
